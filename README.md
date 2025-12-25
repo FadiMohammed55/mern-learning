983d1be237c3ade04ce688937f8ead6f1fdf17e3
FadiMohammed55 <fadimohammed65@gmail.com>
2025-12-25 15:32:17 +0200

    Day 3: arrays and objects

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # MERN Learning Journey
 Day 1: Setup & Basics
 Day 2: JavaScript fundamentals
+Day 3: arrays and objects
--- index.js
+++ index.js
@@ -23,6 +23,31 @@ const checkAge = (age) => {
   return age >= 18 ? "Adult" : "Minor";
 };
 
+const numbers = [1, 2, 3, 4, 5];
+
+const doubled = numbers.map((num) => num * 2);
+console.log(doubled);
+
+const even = numbers.filter((num) => num % 2 === 0);
+console.log(even);
+
+const username = {
+  firstname: "Fadi",
+  lastname: "Mohammed",
+  age: 24,
+  skills: ["MongoDB", "Express", "React", "Node.js"],
+};
+
+console.log(username.firstname);
+
+const users = [
+  { name: "Ali", age: 20 },
+  { name: "Sara", age: 17 },
+];
+
+const adults = users.filter((u) => u.age >= 18);
+console.log(adults);
+
 console.log(checkAge(25));
 
 console.log(`My name is ${name} and I am a ${role}`);

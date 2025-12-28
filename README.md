3a52b32246b5fa03002d25bde8455a9531cd90c5
FadiMohammed55 <fadimohammed65@gmail.com>
2025-12-28 15:39:46 +0200

    Day 9: Express routes and REST API

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Day 5: Fetch API & API interaction
 Day 6: Complete vanilla JS task manager project
 Day 7: Review, refactor and clean code
 Day 8: Node.js backend setup
+Day 9: Express routes and REST API
--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,27 @@ const app = express();
 
 const PORT = 2001;
 
+let tasks = [];
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Srever is running");
+});
+
+app.get("/api/tasks", (req, res) => {
+  res.json(tasks);
+});
+
+app.post("/api/tasks", (req, res) => {
+  const task = req.body;
+  tasks.push(task);
+  res.status(201).json(task);
+});
+
+app.delete("/api/tasks/:index", (req, res) => {
+  tasks.splice(req.params.index, 1);
+  res.json({ message: "Task deleted" });
 });
 
 app.listen(PORT, () => {
